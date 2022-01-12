7b57b2b4d45852d007d0db588201f3d346470113
Zuhaib <theshahzuhaib96@gmail.com>
2022-01-12 17:48:33 +0530

    Add room name, active username and sidebar styles

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,13 +1,14 @@
-// load Xc32 dirname
-const generateMessage = (text) => {
+const generateMessage = (username, text) => {
    return {
+      username,
       text,
       createdAt: new Date().getTime(),
    };
 };
 
-const generateLocationMessage = (url) => {
+const generateLocationMessage = (username, url) => {
    return {
+      username,
       url,
       createdAt: new Date().getTime(),
    };
